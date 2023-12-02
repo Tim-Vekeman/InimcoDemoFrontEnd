0250033e938d72c4d2fc69bdbe385688fd8ac5e4
Tim Vekeman <tim.vekeman@student.odisee.be>
2023-12-02 17:56:20 +0100

    Adding an id to person class

--- src/app/models/person/person.ts
+++ src/app/models/person/person.ts
@@ -1,12 +1,14 @@
 import { SocialMediaAccount } from "../social-media-account/social-media-account"
 
 export class Person {
+    id : number
     firstname : string
     lastname : string
     social_skills : string[]
     social_media_accounts : SocialMediaAccount[]
 
-    constructor(firstname : string, lastname : string, social_skills : string[], social_media_accounts : SocialMediaAccount[]) {
+    constructor(firstname : string, lastname : string, social_skills : string[], social_media_accounts : SocialMediaAccount[], id? : number) {
+        this.id = id ?? 0
         this.firstname = firstname
         this.lastname = lastname
         this.social_skills = social_skills
