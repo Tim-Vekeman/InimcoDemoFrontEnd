9f277df4baba72a54823017cbd1d03a86c77a7c2
Tim Vekeman <tim.vekeman@student.odisee.be>
2023-12-02 16:23:44 +0100

    Adding fields and constructor to socialmediaccount

--- src/app/models/social-media-account/social-media-account.ts
+++ src/app/models/social-media-account/social-media-account.ts
@@ -1,2 +1,9 @@
 export class SocialMediaAccount {
+    type : string;
+    Address : string;
+
+    constructor(type : string, Address : string) {
+        this.type = type;
+        this.Address = Address;
+    }
 }
