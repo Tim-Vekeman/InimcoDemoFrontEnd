a71abb15a9e3e8624808bf1deb786f1c73dac2eb
Tim Vekeman <tim.vekeman@student.odisee.be>
2023-12-02 16:41:03 +0100

    Adding routing to show the form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PersonFormComponent } from './person/components/person-form/person-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/new', pathMatch: 'full'},
+  {path: 'new', component: PersonFormComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
