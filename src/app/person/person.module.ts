90a655cb23b6de296fc57d10925adad018a6597f
Tim Vekeman <tim.vekeman@student.odisee.be>
2023-12-02 15:46:17 +0100

    Adding the "person-form" component

--- src/app/person/person.module.ts
+++ src/app/person/person.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PersonFormComponent } from './components/person-form/person-form.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PersonFormComponent
+  ],
   imports: [
     CommonModule
   ]
