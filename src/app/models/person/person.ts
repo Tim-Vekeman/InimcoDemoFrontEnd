a5a946ac72697163b6143f2a6f199981e560d719
Tim Vekeman <tim.vekeman@student.odisee.be>
2023-12-02 16:25:19 +0100

    Adding the fields and constructor to person

--- src/app/models/person/person.ts
+++ src/app/models/person/person.ts
@@ -1,2 +1,16 @@
+import { SocialMediaAccount } from "../social-media-account/social-media-account"
+
 export class Person {
+    firstname : string
+    lastname : string
+    social_skills : string[]
+    social_media_accounts : SocialMediaAccount[]
+
+    constructor(firstname : string, lastname : string, social_skills : string[], social_media_accounts : SocialMediaAccount[]) {
+        this.firstname = firstname
+        this.lastname = lastname
+        this.social_skills = social_skills
+        this.social_media_accounts = social_media_accounts
+    }
 }
+
