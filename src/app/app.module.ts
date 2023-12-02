e3d19f215b90da6061eb3fe96cb7be2749a2ab64
Tim Vekeman <tim.vekeman@student.odisee.be>
2023-12-02 15:56:35 +0100

    Managing imports and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
+// Angular imports
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
+// App imports
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Module imports
+import { PersonModule } from './person/person.module';
+
+// Module declaration
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    PersonModule
   ],
   providers: [
     provideClientHydration()
