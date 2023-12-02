dcfb2f3e2a7044f3c1606fd390528ce2f4e62283
Tim Vekeman <tim.vekeman@student.odisee.be>
2023-12-02 16:20:51 +0100

    Adding a socialMediaAccount class

---
+++ src/app/models/social-media-account.spec.ts
@@ -0,0 +1,7 @@
+import { SocialMediaAccount } from './social-media-account';
+
+describe('SocialMediaAccount', () => {
+  it('should create an instance', () => {
+    expect(new SocialMediaAccount()).toBeTruthy();
+  });
+});
