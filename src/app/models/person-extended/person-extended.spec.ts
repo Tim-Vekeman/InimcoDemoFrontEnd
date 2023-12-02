18675bec0c27828c6259af784e156729f8a3d3cf
Tim Vekeman <tim.vekeman@student.odisee.be>
2023-12-02 17:58:17 +0100

    Adding a personExtened class

---
+++ src/app/models/person-extended/person-extended.spec.ts
@@ -0,0 +1,7 @@
+import { PersonExtended } from './person-extended';
+
+describe('PersonExtended', () => {
+  it('should create an instance', () => {
+    expect(new PersonExtended()).toBeTruthy();
+  });
+});
