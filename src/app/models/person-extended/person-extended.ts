988fb7dc5741245c4daae35a19a640718fc13d7f
Tim Vekeman <tim.vekeman@student.odisee.be>
2023-12-02 18:12:24 +0100

    Adding the fields to the extended person class

--- src/app/models/person-extended/person-extended.ts
+++ src/app/models/person-extended/person-extended.ts
@@ -1,2 +1,19 @@
-export class PersonExtended {
+import {Person} from '../person/person';
+import { SocialMediaAccount } from '../social-media-account/social-media-account';
+
+export class PersonExtended extends Person{
+    vowels : number;
+    constenants : number;
+    fullname : string;
+    reversed_fullname : string;
+    person_as_json : string;
+
+    constructor(vowels : number, constenants : number, fullname : string, reverse_fullname : string, person : Person) {
+        super(person.firstname, person.lastname, person.social_skills, person.social_media_accounts, person.id)
+        this.vowels = vowels;
+        this.constenants = constenants;
+        this.fullname = fullname;
+        this.reversed_fullname = reverse_fullname;
+        this.person_as_json = JSON.stringify(person);
+    }
 }
