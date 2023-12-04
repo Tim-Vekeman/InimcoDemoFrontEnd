7bc56f016857c88ce21f7abc19b4747c409c09a9
Tim Vekeman <tim.vekeman@student.odisee.be>
2023-12-04 11:09:59 +0100

    adding a routing pad for the new component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
+// angular imports
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+// component imports
 import { PersonFormComponent } from './person/components/person-form/person-form.component';
+import { PersonViewComponent } from './person/components/person-view/person-view.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/new', pathMatch: 'full'},
   {path: 'new', component: PersonFormComponent},
+  {path: 'view/:id', component: PersonFormComponent}
 ];
 
 @NgModule({
